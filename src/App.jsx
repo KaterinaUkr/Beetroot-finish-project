281485d7c282424e28f42d029a061f3e97689715
KaterinaUkr <sharavarova@gmail.com>
2023-06-11 21:56:21 +0300

    add axios API

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Attractions } from './Components/Attractions/Attractions';
 import { Footer } from './Components/Footer/Footer';
 import { Fullscreen } from './Components/Fullscreen/Fullscreen';
 import { Header } from './Components/Header/Header';
-import { Restaurant } from './Components/Restaurant/Restaurant';
+// import { Restaurant } from './Components/Restaurant/Restaurant';
 import { Interesting } from './Components/Interesting/Interesting';
 import { Video } from './Components/Video/Video';
 
@@ -21,7 +21,7 @@ function App() {
       {/* <AttractionItem></AttractionItem> */}
       {/* <AllAttraction></AllAttraction> */}
       {/* <Video></Video> */}
-      <Restaurant></Restaurant>
+      {/* <Restaurant></Restaurant> */}
       <Interesting></Interesting>
       <Footer></Footer>
     
--- src/Components/Restaurant/Restaurant.jsx
+++ src/Components/Restaurant/Restaurant.jsx
@@ -1,24 +1,13 @@
 // import './Restaurant.scss'
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, setState } from "react";
 
 import axios from 'axios';
 
-// export const Blog = () => {
-// 	const [BlogData, setBlogData] = useState([])
-
-// 	useEffect(() => {
-// 		axios('https://jsonplaceholder.typicode.com/posts').then(res => setBlogData(res.data))
-// 	}, []);
-
-// 	return <div className="blog">
-// 			{BlogData.map(item => <Blogitem key={item.id} {...item}></Blogitem>)}
-// 		</div>
-
-// };
-
 export const Restaurant = () => {
-	const options = {
+const [rests] = useState([])
+	
+const options = {
 		method: 'GET',
 		url: 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants',
 		params: {
@@ -30,64 +19,28 @@ export const Restaurant = () => {
 		}
 	};
 
-	axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-	});
-	
-return (
-			<div>
-				<h2>Here must be a restaurant</h2>
-				{/* {Rests.length > 0 && ( */}
-				{/* <ul> */}
-					
-						{Rests.map(item => (
-							<li>Name:{item[1].name}</li>
-						))}
-					{/* </ul> */}
-				{/* )} */}
-			</div>
-		);
+	useEffect(() => {
+		axios
+		.request(options)				
+		.then((response) => {			
+			console.log(response.data.data);
+		})			
+		.catch(function (error) {
+			console.error(error);
+		});
+	}, []);
 
-
-
-}
-
-// запрос через Rapidapi
-// export const Restaurant = () => {
-// 	const [Rests, setRestData] = useState([])
-// 	const options = {
-// 				method: 'GET',
-// 				headers: {
-// 					'X-RapidAPI-Key': 'c09e7890damsh665bf2a698decc9p18922cjsn8b4b6c5b6ec1',
-// 					'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
-// 				}	}
-		
-// 		useEffect(() => {
-// 			fetch("https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=295369", options)
-// 				.then(response => response.json())				
-// 				.then(response => console.log(response.data.data))
-// 				.catch(err => console.error(err))
-// 		}, [])
-		
-			
-// 		return (
-// 			<div>
-// 				<h2>Here must be a restaurant</h2>
-// 				{/* {Rests.length > 0 && ( */}
-// 				{/* <ul> */}
-					
-// 						{Rests.map(item => (
-// 							<li>Name:{item[1].name}</li>
-// 						))}
-// 					{/* </ul> */}
-// 				{/* )} */}
-// 			</div>
-// 		);
-// 	}
+	return (
+	<div>
+			<h2>Here must be a restaurant</h2>
+				<ul>
+ 						{rests.map(rest =>
+									<li key={rest.restaurantsId}>{rest.name}</li>
+								)
+						}
+					</ul>		
+		</div>
+	)
+};
 
     
